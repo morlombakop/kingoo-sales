e369a71a1e6c5dfbe81238eeb25f17fb9bcff629
Stephane, Mbakop (Malaysia Kuala Lumpur) <stephane.ngongangmorlo@baesystems.com>
2019-07-18 15:52:17 +0800

    Initial Commit

---
+++ app/src/main/java/com/morlombakop/kingoosales/MainActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * Created by Mbakop Ngongang Morlo on 7/17/19 4:12 PM
+ * Copyright (c) 2019 . All rights reserved.
+ * Last modified 7/17/19 4:06 PM
+ */
+
+package com.morlombakop.kingoosales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
